1e13a957c1de169ee170461eca3e32c80acfc0c2
HeavensLanding <rasmussencourtney0103@gmail.com>
2024-06-21 19:49:24 -0600

    member class

--- w8_assignment.js
+++ w8_assignment.js
@@ -1,18 +1,28 @@
+class Member {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    describe() {
+        return `${this.name} is ${this.age} year(s) old.`
+    }
+}
+
 class Family {
     constructor(name) {
         this.name = name;
         this.members = [];
     }
 
-    addPlayer(player) {
-        if (player instanceof Player) {
-            this.players.push(player);
+    addMember(member) {
+        if (member instanceof Member) {
+            this.members.push(member);
         } else {
-            throw new Error(`You can only add an instance of Player. Argument is not a player: ${player}`);
+            throw new Error(`You can only add an instance of Member. Argument is not a member: ${member}`);
         }
     }
 
     describe() {
-        return`${this.name} has ${this.players.lengths} players.`;
+        return`${this.name} has ${this.members.lengths} family members.`;
     }
 }
